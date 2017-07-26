3b57048e6672be702a9399be35e790cbc30f7ab8
Ecohnoch <542305306@qq.com>
2017-07-26 23:52:16 +0800

    完善了模块化

--- Markdown/setup.py
+++ Markdown/setup.py
@@ -5,7 +5,8 @@ from distutils.core import setup
 setup(
     name         =    'ecohMD',
     version      =    '1.0.0',
-    py_modules   =    ['ecohMD'],
+    py_modules   =    ['ecohMD', 'readItems', 'judge', 'parse'],
+    data_files   =    ['codeJS.css', 'codeStyle.css', 'quoteStyle.css', 'prism.css', 'prism.js'],
     author       =    'Ecohnoch',
     author_email =    '542305306@qq.com',
     url          =     'www.ecohnoch.com',
